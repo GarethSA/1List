b82ee8057e3a54adb20ff6f29af56300dc93ea3e
GarethSA <sajuice1@gmail.com>
2026-04-30 11:09:48 +0200

    Update color definitions in DarkColors.kt

--- core/designsystem/src/main/kotlin/com/lolo/io/onelist/core/designsystem/colors/DarkColors.kt
+++ core/designsystem/src/main/kotlin/com/lolo/io/onelist/core/designsystem/colors/DarkColors.kt
@@ -12,8 +12,11 @@ fun darkColorScheme(): ColorScheme = materialDarkColorScheme(
     secondary = Palette.NAVY_LIGHT,
     tertiary = Palette.NAVY,
     background = Palette.PURE_BLACK,
+    onBackground = Palette.PURE_WHITE,
+    surface = Palette.PURE_BLACK,
+    onSurface = Palette.PURE_WHITE,
     error = Palette.NAVY_LIGHT,
-    outline = Palette.GRAY,
+    outline = Palette.GRAY_DARK,
     outlineVariant = Palette.GRAY_DARK,
-    surfaceContainer = Palette.NAVY_DARK,
+    surfaceContainer = Palette.SURFACE_BLACK,
 )
